0e9abadacfa47e89188ea9d94c07bde0a0708354
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-12 15:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-var kittens = ['Milo','Otis','Garfield'];
+kittens = ['Milo','Otis','Garfield'];
 var destructivelyAppendKitten = function(name){
   kittens.push(name);
   return kittens
